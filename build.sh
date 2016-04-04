3871ea11c3d1b3175b1dca32926f81d895e05158
Thomas Orozco <thomas@orozco.fr>
2016-04-04 19:53:06 +0200

    Optimize Dockerfile for faster local builds

--- build.sh
+++ build.sh
@@ -6,34 +6,11 @@ set -o nounset
 # - BUILD_HOME
 # - DEPLOY_TO
 
-CADVISOR_BASE_VERSION="v0.22.2"
-
-GODIST="${BUILD_HOME}/godist"
-export GOROOT="${GODIST}/go"
-export GOPATH="${BUILD_HOME}/gopath"
-export PATH="${GOPATH}/bin:${GOROOT}/bin:${PATH}"
-
-GOLANG_VERSION=1.6
-GOLANG_DOWNLOAD_URL="https://golang.org/dl/go${GOLANG_VERSION}.linux-amd64.tar.gz"
-GOLANG_DOWNLOAD_SHA256="5470eac05d273c74ff8bac7bef5bad0b5abbd1c4052efbdbc8db45332e836b0b"
-GO_DEPS=(github.com/tools/godep)
-
-curl -fsSL "$GOLANG_DOWNLOAD_URL" -o golang.tar.gz
-echo "$GOLANG_DOWNLOAD_SHA256  golang.tar.gz" | sha256sum -c -
-
-mkdir -p "$GODIST"
-tar -C "$GODIST" -xzf golang.tar.gz
-rm golang.tar.gz
-
-# Get Go build dependencies
-go get "${GO_DEPS[@]}"
-
-# Get cadvisor
-CADVISOR="github.com/google/cadvisor"
-go get -d "$CADVISOR" || true  # Usually an error about an expected import that doesn't matter..!
-cd "${GOPATH}/src/${CADVISOR}"
+# shellcheck source=helper.inc
+. "${BUILD_HOME}/helper.inc"
 
 # Check out what we're building, and apply our patch series
+cd "${GOPATH}/src/${CADVISOR}"
 git checkout "$CADVISOR_BASE_VERSION"
 
 for patchfile in "${BUILD_HOME}/patches/"*.patch; do
